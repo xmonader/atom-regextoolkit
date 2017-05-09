a9deea12bfb8491762597f15c3b463670df5cd87
Ahmed T. Youssef <xmonader@gmail.com>
2017-05-09 19:26:15 +0200

    init

---
+++ spec/atom-regextoolkit-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomRegextoolkitView from '../lib/atom-regextoolkit-view';
+
+describe('AtomRegextoolkitView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
